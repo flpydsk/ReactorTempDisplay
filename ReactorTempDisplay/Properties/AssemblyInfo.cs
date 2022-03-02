730e4af6e28958544150548e816a2c8210bff5cd
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2022-03-01 16:58:15 -0800

    Add project files.

---
+++ ReactorTempDisplay/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyTitle("ReactorTempDisplay")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ReactorTempDisplay")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("a64c26e2-d648-4d00-8620-b0ff1ecde94a")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
