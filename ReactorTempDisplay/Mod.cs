67574465b0cdf5d59312db848723a118d02c816f
FloppyDisk <FloppyDisk@none>
2023-03-11 23:13:01 -0800

    Cleaned up code from decompile and fixed checks

--- ReactorTempDisplay/Mod.cs
+++ ReactorTempDisplay/Mod.cs
@@ -2,27 +2,22 @@
 
 namespace ReactorTempDisplay
 {
-    // Token: 0x02000003 RID: 3
     public class Mod : PulsarMod
 	{
-		// Token: 0x06000003 RID: 3 RVA: 0x00002180 File Offset: 0x00000380
+
 		public override string HarmonyIdentifier()
 		{
-			return "FLoppyDisk.ReactorTempDisplay";
+			return "FoppyDisk.ReactorTempDisplay";
 		}
 
-		// Token: 0x17000001 RID: 1
-		// (get) Token: 0x06000004 RID: 4 RVA: 0x00002197 File Offset: 0x00000397
 		public override string Version
 		{
 			get
 			{
-				return "0.1.10";
+				return "0.1.11";
 			}
 		}
 
-		// Token: 0x17000002 RID: 2
-		// (get) Token: 0x06000005 RID: 5 RVA: 0x0000219E File Offset: 0x0000039E
 		public override string Author
 		{
 			get
@@ -31,8 +26,6 @@ namespace ReactorTempDisplay
 			}
 		}
 
-		// Token: 0x17000003 RID: 3
-		// (get) Token: 0x06000006 RID: 6 RVA: 0x000021A5 File Offset: 0x000003A5
 		public override string Name
 		{
 			get
@@ -41,8 +34,6 @@ namespace ReactorTempDisplay
 			}
 		}
 
-		// Token: 0x17000004 RID: 4
-		// (get) Token: 0x06000007 RID: 7 RVA: 0x000021AC File Offset: 0x000003AC
 		public override string ShortDescription
 		{
 			get
@@ -51,13 +42,11 @@ namespace ReactorTempDisplay
 			}
 		}
 
-		// Token: 0x17000005 RID: 5
-		// (get) Token: 0x06000008 RID: 8 RVA: 0x000021B3 File Offset: 0x000003B3
 		public override string LongDescription
 		{
 			get
 			{
-				return "Licence: Anyone may use this mod or re-distribute this mod in its origional form with credit. Modification is allowed with concent from FloppyDisk via discord or email flpydsk@pm.me";
+				return "Licence: GPL 3.0 only";
 			}
 		}
 	}
---
+++ ReactorTempDisplay/ReactorTempDisplay.cs
@@ -0,0 +1,31 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace ReactorTempDisplay
+{
+
+    [HarmonyPatch(typeof(PLInGameUI), "Update")]
+	internal class ReactorTempDisplay
+	{
+		public static void Postfix(PLInGameUI __instance)
+		{
+			//If we are in a game then override the temp bar
+			if (PLServer.Instance && PLGlobal.Instance && PLNetworkManager.Instance && PLNetworkManager.Instance.ViewedPawn)
+			{
+				__instance.TempFill.rectTransform.SetSizeWithCurrentAnchors(0, 110f);
+				int Stability = Mathf.RoundToInt(Mathf.Clamp01(1f - PLEncounterManager.Instance.PlayerShip.CoreInstability) * 100f);
+				bool IsStable = Stability < 100;
+				string DisplayText;
+				if (IsStable)
+				{
+					DisplayText = "S:" + Stability.ToString("000") + "%";
+				}
+				else
+				{
+					DisplayText = "T:" + Mathf.RoundToInt(PLEncounterManager.Instance.PlayerShip.MyStats.ReactorTempCurrent / PLEncounterManager.Instance.PlayerShip.MyStats.ReactorTempMax * 100f).ToString("000") + "%";
+				}
+				PLGlobal.SafeLabelSetText(__instance.TempValueLabel, DisplayText);
+			}
+		}
+	}
+}
