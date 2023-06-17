9bc4dff59cb38c51da13539ba18975623af53085
FloppyDisk <FloppyDisk@no.no>
2023-06-17 18:35:38 +1200

    Fix No claim exeptions, cleanup of code, fix lic

--- ReactorTempDisplay/Mod.cs
+++ ReactorTempDisplay/Mod.cs
@@ -1,16 +1,32 @@
-﻿//Copyright 2023 (c) Floppydisk
-//GPL 3.0-only
+﻿/*
+    Reactor Temp Display, Displays the reactor temperature and stability in the HUD for Pulsar Lost Colony.
+    Copyright (C) 2023 FloppyDisk
+    https://github.com/flpydsk/ReactorTempDisplay.git
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 using PulsarModLoader;
 
 namespace ReactorTempDisplay
 {
     public class Mod : PulsarMod
 	{
-		public override string Version => "1.14";
+		public override string Version => "1.16";
 		public override string Author => "FloppyDisk";
 		public override string Name => "ReactorTempDisplay";
         public override string HarmonyIdentifier() => $"{Author}.{Name}";
         public override string ShortDescription => "Reactor Temperature & Stability Display";
-		public override string LongDescription => "Licence: GPL 3.0 only";
+		public override string LongDescription => "Licence: GNU GPL-3.0";
     }
 }
--- ReactorTempDisplay/ReactorTempDisplay.cs
+++ ReactorTempDisplay/ReactorTempDisplay.cs
@@ -1,56 +1,86 @@
-﻿//Copyright 2023 (c) Floppydisk
-//GPL 3.0-only
+﻿/*
+    Reactor Temp Display, Displays the reactor temperature and stability in the HUD for Pulsar Lost Colony.
+    Copyright (C) 2023 FloppyDisk
+    https://github.com/flpydsk/ReactorTempDisplay.git
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 using HarmonyLib;
 using PulsarModLoader;
 using UnityEngine;
 
 namespace ReactorTempDisplay
 {
-
     [HarmonyPatch(typeof(PLInGameUI), "Update")]
     internal class ReactorTempDisplay
     {
+        //Init mode save and create a copy
         public static SaveValue<bool> SavedDisplayType = new SaveValue<bool>("DisplayType", false);
         public static bool DisplayType = SavedDisplayType.Value;
+
+        private static float BarLength;
+        private static string DisplayText;
+        private static int Stability;
+        private static int Temperature;
+
         public static void Postfix(PLInGameUI __instance)
         {
-
-            //If we are in a game then override the temp bar
+            //If we are in a game modify the UI
             if (PLServer.Instance && PLGlobal.Instance && PLNetworkManager.Instance && PLNetworkManager.Instance.ViewedPawn)
             {
-                string TempText = "T:" + Mathf.RoundToInt(PLEncounterManager.Instance.PlayerShip.MyStats.ReactorTempCurrent / PLEncounterManager.Instance.PlayerShip.MyStats.ReactorTempMax * 100f).ToString("000") + "%";
-                int Stability = Mathf.RoundToInt((1f - PLEncounterManager.Instance.PlayerShip.CoreInstability) * 100f);
-                string StabilityText = "S:" + Stability.ToString("000") + "%";
-                bool IsNotStable = Stability < 100;
-                string DisplayText;
-
-                //Depending on display mode, display correct option
-                if (DisplayType)
+                //Check we have a claimed ship else display 0
+                if (PLEncounterManager.Instance.PlayerShip)
                 {
-                    //Displays in short mode, switches baste on status of reactor
-                    BarLength = 115f;
-                    if (IsNotStable)
-                    {
-                        DisplayText = StabilityText;
-                    }
-                    else
-                    {
-                        DisplayText = TempText;
-                    }
+                    Temperature = Mathf.RoundToInt(PLEncounterManager.Instance.PlayerShip.MyStats.ReactorTempCurrent / PLEncounterManager.Instance.PlayerShip.MyStats.ReactorTempMax * 100f);
+                    Stability = Mathf.RoundToInt((1f - PLEncounterManager.Instance.PlayerShip.CoreInstability) * 100f);
                 }
                 else
                 {
-                    //display all at the same time
-                    BarLength = 235f;
-                    DisplayText = TempText + " | " + StabilityText;
-
+                    Stability = 0;
+                    Temperature = 0;
                 }
 
-                // set size of bar baste on 
+                //Put the temperature and stability values in to a string format
+                string StabilityText = "S:" + Stability.ToString("000") + "%";
+                string TempText = "T:" + Temperature.ToString("000") + "%";
+                bool IsNotStable = Stability < 100;
+
+                //Display mode from selection.
+                if (DisplayType)
+                    {
+                        //Display short mode
+                        BarLength = 115f;
+                        if (IsNotStable)
+                        {
+                            DisplayText = StabilityText;
+                        }
+                        else
+                        {
+                            DisplayText = TempText;
+                        }
+                    }
+                    else
+                    {
+                        //Display full mode
+                        BarLength = 235f;
+                        DisplayText = TempText + " | " + StabilityText;
+                    }
+
+                //Modify UI element size and text
                 __instance.TempFill.rectTransform.SetSizeWithCurrentAnchors(0, BarLength);
                 PLGlobal.SafeLabelSetText(__instance.TempValueLabel, DisplayText);
             }
         }
-        private static float BarLength;
     }
 }
